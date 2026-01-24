7123078872107910d88faaf35d52bc9e9128939d
theun1c <belovdima1122@yandex.ru>
2026-01-25 00:08:06 +0300

    add some base logic for logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, io::Write, time::SystemTime};
 
 // logger path
 const LOG_PATH: &str = "./logger/logger_file.txt";
@@ -12,18 +12,24 @@ fn create_logger_file(){
 }
 
 // red highlight 
-fn error_log(){
-
+fn error_log(text: &str, mut file: File){
+    let time = SystemTime::now();
+    let format_str = format!("{:?} | ERROR | {}", time, text);
+    file.write_all(format_str.as_bytes());
 }
 
 // greem highlight
-fn succesful_log(){
-
+fn succesful_log(text: &str, mut file: File){
+    let time = SystemTime::now();
+    let format_str = format!("{:?} | WARN | {}", time, text);
+    file.write_all(format_str.as_bytes());
 }
 
 // yellow highlight
-fn warn_log(){ 
-
+fn warn_log(text: &str, mut file: File){ 
+    let time = SystemTime::now();
+    let format_str = format!("{:?} | SUCCSES | {}", time, text);
+    file.write_all(format_str.as_bytes());
 }
 
 fn main() { 
