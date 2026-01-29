c1acc7c40e4c8a9befbc4ba1a41147141768c77c
theun1c <belovdima1122@yandex.ru>
2026-01-30 00:14:33 +0300

    update fonts (i cannot do hand drawn style....cry)

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,6 @@
 mod logger;
 use yew::prelude::*;
 
-#[derive(Clone, PartialEq)]
-struct Video {
-    title: AttrValue, 
-    url: AttrValue
-}
-
-#[derive(Properties, PartialEq)]
-struct  VideoListProps {
-    videos: Vec<Video>,
-    on_click: Callback<Video>
-}
-
-#[derive(Properties, PartialEq)]
-struct VideosDetailsProps {
-    video: Video,
-}
-
-#[component]
-fn VideoDetails(VideosDetailsProps { video}: &VideosDetailsProps) -> Html {
-    html! {
-        <div>
-            <h3>{ &*video.title }</h3>
-            <h5>{ &*video.url }</h5>
-        </div>
-    }
-}
-
-#[component]
-fn VideoList(VideoListProps { videos , on_click}: &VideoListProps) -> Html {
-    let on_selected = |video: &Video| {
-        let on_click = on_click.clone();
-        let video = video.clone();
-        Callback::from(move |_| {
-            on_click.emit(video.clone())
-        })
-    };
-    html!{
-        for video in videos {
-            <p onclick={on_selected(video)}> { format!("{} {}", video.title, video.url) } </p>
-        }
-    }
-}
-
-
-
 // 
 // Component for main title 
 #[component]
@@ -73,18 +28,17 @@ fn MainTitle() -> Html {
 fn InputField() -> Html {
     html! {
         <>
-        
             <div class="container">
                 <input type="text" class="text-field_main" placeholder="Book / Author / Pages / Time"/>
             </div>
-
+            
         </>
     }
 }
 
-
 #[component]
 fn App() -> Html {
+
 
     html! {
         <>
@@ -94,40 +48,6 @@ fn App() -> Html {
         </>
     }
 
-    // let videos = vec![
-    //     Video{
-    //         title: "title 1".into(),
-    //         url: "url 1".into()
-    //     },
-    //     Video{
-    //         title: "title 2".into(),
-    //         url: "url 2".into()
-    //     },
-    // ];
-
-    // let selected_video = use_state(|| None);
-    
-    // let on_video_select = {
-    //     let selected_video = selected_video.clone();
-    //     Callback::from(move |video: Video| {
-    //         selected_video.set(Some(video));
-    //     } )
-    // };
-
-
-    // html! {
-    //     <>
-    //         <h1 class="test">{"Hello, world!"}</h1>
-
-    //         <div>
-    //             <VideoList {videos} on_click={on_video_select}/> 
-    //         </div>
-
-    //         if let Some(video) = &*selected_video {
-    //             <VideoDetails video={video.clone()}/>
-    //         }
-    //     </>
-    // }
 }
 
 
